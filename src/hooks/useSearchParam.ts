5c47b4e41fcffe2d4757275364b15269a5a15ff7
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-07-14 16:48:54 +0300

    eslint fix

--- src/hooks/useSearchParam.ts
+++ src/hooks/useSearchParam.ts
@@ -1,6 +1,6 @@
-import {useSearchParams} from "next/navigation";
-import getSearchParamValue from "../searchParams/getSearchParamValue";
-import {ParameterOptions} from "../types";
+import {useSearchParams} from "next/navigation"
+import getSearchParamValue from "../searchParams/getSearchParamValue"
+import {ParameterOptions} from "../types"
 
 const useSearchParam = <T, >(opts: Pick<ParameterOptions<T>, 'decode' | 'name'>) => {
     return getSearchParamValue(useSearchParams(), opts)

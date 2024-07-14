602cfc04c40c0259f886f01417e1792d01bc7cfe
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-07-14 03:15:03 +0300

    Fix the rest of exports.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,27 @@
 
 export {default as getLinkQueryValue} from './link/getLinkQueryValue'
 export {default as setLinkQueryValue} from './link/setLinkQueryValue'
-export {default as createLinker} from './createLinker'
 
+export {default as getQueryParamValue} from './query/getQueryParamValue'
+export {default as setQueryParamValue} from './query/setQueryParamValue'
+
+export {default as getSearchParamValue} from './searchParams/getSearchParamValue'
+export {default as setSearchParamValue} from './searchParams/setSearchParamValue'
+
+export {default as createLinker} from './createLinker'
+export {default as useCurrentLink} from './hooks/useCurrentLink'
+export {default as useLinker} from './hooks/useLinker'
+
+
+export {default as decodeBool} from './common/decodeBool'
+export {default as encodeBool} from './common/encodeBool'
+
+export {default as decodeString} from './common/decodeString'
+export {default as encodeString} from './common/encodeString'
+
+
+export {default as decodeNumber} from './common/decodeNumber'
+export {default as encodeNumber} from './common/encodeNumber'
+
+export {default as decodePage} from './common/decodePage'
+export {default as encodePage} from './common/encodePage'

ac72900379c58eb4df8acb241e98243039732625
l-you <l-you@revotale.com>
2025-09-03 14:40:13 +0000

    Polish code

--- src/utils/createLinker.ts
+++ src/utils/createLinker.ts
@@ -15,18 +15,19 @@ function createLinker(link: URL): Linker<URL>
 function createLinker(link: RelativeURL): Linker<RelativeURL>
 function createLinker(link: RelativeURL | URL): Linker<RelativeURL | URL>
 function createLinker(link: RelativeURL | URL): Linker<RelativeURL | URL> {
-    const params = new URLSearchParams(link.search.toString())
+    const search = new URLSearchParams(link.search.toString())
     
     // Shared implementation for both setValue and getValue
     const setValue = <V>(opt: Pick<ParameterOptions<V>, 'name' | 'encode'>, value: V): void => {
-        setSearchParamValue(params, opt, value)
+        setSearchParamValue(search, opt, value)
     }
-    
+
+
     const getValue = <V>({decode, name}: Pick<ParameterOptions<V>, 'name' | 'decode'>): V => 
-        decode(params.getAll(name))
+        decode(search.getAll(name))
     
     if (link instanceof URL) {
-        const getLink = (): URL => new URL(`${link.pathname}${getNormalizedQueryStr(params)}`, link.origin)
+        const getLink = (): URL => new URL(`${link.pathname}${getNormalizedQueryStr(search)}`, link.origin)
         const linker: Linker<URL> = {
             setValue,
             getValue,
@@ -34,18 +35,17 @@ function createLinker(link: RelativeURL | URL): Linker<RelativeURL | URL> {
             toString: () => getLink().toString()
         }
         return linker
-    } else {
-        const linker: Linker<RelativeURL> = {
-            setValue,
-            getValue,
-            getLink: () => ({
-                pathname: link.pathname,
-                search: params,
-                toString: () => `${link.pathname}${getNormalizedQueryStr(params)}`
-            })
-        }
-        return linker
     }
+    const linker: Linker<RelativeURL> = {
+        setValue,
+        getValue,
+        getLink: () => ({
+            pathname: link.pathname,
+            search,
+            toString: () => `${link.pathname}${getNormalizedQueryStr(search)}`
+        })
+    }
+    return linker
 }
 
 export default createLinker

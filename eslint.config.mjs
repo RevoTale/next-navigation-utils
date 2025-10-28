c4fb0cfba4d9bc51a4eb5102827847192f966a2e
l-you <l-you@revotale.com>
2025-10-28 16:24:50 +0000

    Fix eslint config accordin to the react hooks 7

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
 
 import love from 'eslint-config-love'
 import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
 
-export default [
+
+export default defineConfig([
+  {
+    ignores:[
+      './dist/**'
+    ]
+  },
   {
     ...love,
     files: ['./src/**/*.tsx', './src/**/*.ts'],
   },
-   reactHooks.configs['recommended-latest'],
-]
+   reactHooks.configs.flat['recommended-latest'],
+])
